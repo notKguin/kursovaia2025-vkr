200a06c3815a1c239d9b6f8cdfe821d7459e8cfb
notKguin <kvitonovmaksim1@gmail.com>
2025-12-26 10:12:22 +0500

    update app.py

--- app.py
+++ app.py
@@ -196,15 +196,16 @@ col1, col2, col3, col4 = st.columns(4)
 with col1:
     st.subheader("Logistic Regression")
     model_block("Logistic Regression", models["Logistic Regression"])
-
+    st.divider()
 with col2:
     st.subheader("Random Forest")
     model_block("Random Forest", models["Random Forest"])
-
+    st.divider()
 with col3:
     st.subheader("Gradient Boosting")
     model_block("Gradient Boosting", models["Gradient Boosting"])
-
+    st.divider()
 with col4:
     st.subheader("MLP (Neural Network)")
     model_block("MLP (Neural Network)", models["MLP (Neural Network)"])
+    st.divider()
